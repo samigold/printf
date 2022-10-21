3038a2d255a372eac80ea7ed1d7c616a37dd4905
samigold <51761415+samigold@users.noreply.github.com>
2022-10-21 23:08:26 +0100

    Update converter.c

--- converter.c
+++ converter.c
@@ -1,27 +1,34 @@
 #include "main.h"
+/**
+ * _print_octal - prints a number converted into octal
+ * @n: unsigned integer
+ * @p: pointer to number of times buffer has been printed
+ * @count: chars printed
+ * @buffer: string allocated in the heap where chars are kept until printing
+ * Return: Counter for number of chars printed
+ */
+
+int _print_octal(int *p, unsigned int n, int count, char *buffer)
+{
+	if (n / 8)
+		count = _print_octal(p, n / 8, count, buffer);
+	count = _write_char(p, n % 8 + '0', buffer, count);
+	return (count);
+}
 
 /**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * _print_binary - prints a number converted into binary
+ * @n: unsigned integer
+ * @count: chars printed
+ * @p: pointer to number of times buffer has been printed
+ * @buffer: string allocated in the heap where chars are kept until printing
+ * Return: Counter for number of chars printed
  */
-char *convert(unsigned long int num, int base, int lowercase)
+int _print_binary(int *p, unsigned int n, int count, char *buffer)
 {
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
+	if (n / 2)
+		count = _print_binary(p, n / 2, count, buffer);
+	count = _write_char(p, n % 2 + '0', buffer, count);
 
-	rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-	do {
-		*--ptr = rep[num % base];
-		num /= base;
-	} while (num != 0);
-
-	return (ptr);
+	return (count);
 }
